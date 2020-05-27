cdce3ce48511149f0650629fa6ab85ffc40155cb
Raymond Sambur <raymond.sambur@dana.id>
2020-05-27 14:49:26 +0700

    First Commit

---
+++ src/main/java/project/functions/PageFunctions.java
@@ -0,0 +1,17 @@
+package project.functions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static project.androidDriver.AndroidDriverInstance.androidDriver;
+
+public class PageFunctions {
+
+    public WebElement waitABit(By element) {
+        WebDriverWait wait = new WebDriverWait(androidDriver, 50);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(element));
+    }
+
+}
---
+++ src/test/java/project/CucumberRunner.java
@@ -0,0 +1,16 @@
+package project;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        stepNotifications = true,
+        //for plugin check https://cucumber.io/docs/cucumber/reporting/
+        plugin = {"json:build/cucumber.json", "pretty", "html:build/result"},
+        features = "src/test/resources/features", tags = {""})
+
+public class CucumberRunner {
+}
+

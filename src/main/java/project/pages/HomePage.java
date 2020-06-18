1049ddce73d6819684d4a97ca861528569120341
Reynold Vinson Chen <reynold.vinson@gmail.com>
2020-06-18 16:01:16 +0700

    add Home Page

--- src/main/java/project/pages/HomePage.java
+++ src/main/java/project/pages/HomePage.java
@@ -8,11 +8,11 @@ import static project.androidDriver.AndroidDriverInstance.androidDriver;
 public class HomePage implements HomeLocator {
     public boolean isOnHomePage() { return PageFunctions.waitABit(IMAGE_DANA).isDisplayed(); }
 
-    public void tapTopUp() { return androidDriver.findElement(BUTTON_TOP_UP).click(); }
-    public void tapHistoryTransaction() { return androidDriver.findElement(BUTTON_HISTORY).click(); }
-    public void tapMyAccount() { return androidDriver.findElement(BUTTON_MY_ACCOUNT).click(); }
-    public void tapScanToPay() { return androidDriver.findElement(BUTTON_SCAN_TO_PAY).click(); }
-    public void tapTransfer() { return androidDriver.findElement(BUTTON_TRANSFER).click(); }
+    public void tapTopUp() { androidDriver.findElement(BUTTON_TOP_UP).click(); }
+    public void tapHistoryTransaction() { androidDriver.findElement(BUTTON_HISTORY).click(); }
+    public void tapMyAccount() { androidDriver.findElement(BUTTON_MY_ACCOUNT).click(); }
+    public void tapScanToPay() { androidDriver.findElement(BUTTON_SCAN_TO_PAY).click(); }
+    public void tapTransfer() { androidDriver.findElement(BUTTON_TRANSFER).click(); }
 
     public String viewBalance() { return androidDriver.findElement(TEXT_BALANCE).getText(); }
 }

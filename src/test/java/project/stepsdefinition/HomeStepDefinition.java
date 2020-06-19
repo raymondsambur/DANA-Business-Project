5c28063c9cceb77268ad6314aea5f1bbac73eb79
Reynold Vinson Chen <reynold.vinson@gmail.com>
2020-06-19 16:51:02 +0700

    add Top Up

--- src/test/java/project/stepsdefinition/HomeStepDefinition.java
+++ src/test/java/project/stepsdefinition/HomeStepDefinition.java
@@ -2,47 +2,41 @@ package project.stepsdefinition;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import project.pages.HomePage;
 
 public class HomeStepDefinition {
+
+    HomePage homePage = new HomePage();
+
     @When("User see DANA Balance on Home Page")
-    public void userSeeDANABalanceOnHomePage() {
-    }
+    public void userSeeDANABalanceOnHomePage() { homePage.viewBalance(); }
 
     @Then("User validate DANA Balance with current DANA Balance on Home Page")
-    public void userValidateDANABalanceWithCurrentDANABalanceOnHomePage() {
-    }
+    public void userValidateDANABalanceWithCurrentDANABalanceOnHomePage() { }
 
     @When("User tap Top Up Button on Home Page")
-    public void userTapTopUpButtonOnHomePage() {
-    }
+    public void userTapTopUpButtonOnHomePage() { homePage.tapTopUp(); }
 
     @When("User tap Transaction Button on Home Page")
-    public void userTapTransactionButtonOnHomePage() {
-    }
+    public void userTapTransactionButtonOnHomePage() { homePage.tapHistoryTransaction(); }
 
     @Then("User is on Transaction page")
-    public void userIsOnTransactionPage() {
-    }
+    public void userIsOnTransactionPage() { }
 
     @When("User tap Account Button on Home Page")
-    public void userTapAccountButtonOnHomePage() {
-    }
+    public void userTapAccountButtonOnHomePage() { homePage.tapMyAccount(); }
 
     @When("User tap Transfer Button on Home Page")
-    public void userTapTransferButtonOnHomePage() {
-    }
+    public void userTapTransferButtonOnHomePage() { homePage.tapTransfer(); }
 
     @Then("User see message {string}")
-    public void userSeeMessage(String arg0) {
-    }
+    public void userSeeMessage(String arg0) { }
 
     @When("User tap QR Scan Button on Home Page")
-    public void userTapQRScanButtonOnHomePage() {
-    }
+    public void userTapQRScanButtonOnHomePage() { homePage.tapScanToPay(); }
 
     @Then("User is on QR Scan page")
-    public void userIsOnQRScanPage() {
-    }
+    public void userIsOnQRScanPage() { }
 
     @When("User tap Notification Button on Home Page")
     public void userTapNotificationButtonOnHomePage() {

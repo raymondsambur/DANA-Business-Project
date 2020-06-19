d8567590dcf4d1be4c9ba04b28e97009e5bf7e61
Reynold Vinson Chen <reynold.vinson@gmail.com>
2020-06-19 15:35:03 +0700

    add Home Page

---
+++ src/test/java/project/stepsdefinition/HomeStepDefinition.java
@@ -0,0 +1,50 @@
+package project.stepsdefinition;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class HomeStepDefinition {
+    @When("User see DANA Balance on Home Page")
+    public void userSeeDANABalanceOnHomePage() {
+    }
+
+    @Then("User validate DANA Balance with current DANA Balance on Home Page")
+    public void userValidateDANABalanceWithCurrentDANABalanceOnHomePage() {
+    }
+
+    @When("User click Top Up Button on Home Page")
+    public void userClickTopUpButtonOnHomePage() {
+    }
+
+    @When("User click Transaction Button on Home Page")
+    public void userClickTransactionButtonOnHomePage() {
+    }
+
+    @Then("User is on Transaction page")
+    public void userIsOnTransactionPage() {
+    }
+
+    @When("User click Account Button on Home Page")
+    public void userClickAccountButtonOnHomePage() {
+    }
+
+    @When("User click Transfer Button on Home Page")
+    public void userClickTransferButtonOnHomePage() {
+    }
+
+    @Then("User see message {string}")
+    public void userSeeMessage(String arg0) {
+    }
+
+    @When("User click QR Scan Button on Home Page")
+    public void userClickQRScanButtonOnHomePage() {
+    }
+
+    @Then("User is on QR Scan page")
+    public void userIsOnQRScanPage() {
+    }
+
+    @When("User click Notification Button on Home Page")
+    public void userClickNotificationButtonOnHomePage() {
+    }
+}

8ee397c6061826c84aea6e96267471ae5b286762
Reynold Vinson Chen <reynold.vinson@gmail.com>
2020-06-23 13:14:39 +0700

    add Top Up Result

---
+++ src/test/java/project/stepsdefinition/TopUpResultStepDefinition.java
@@ -0,0 +1,11 @@
+package project.stepsdefinition;
+
+import io.cucumber.java.en.And;
+
+public class TopUpResultStepDefinition {
+    @And("User tap Back to Home Button on Success Payment Result Page")
+    public void userTapBackToHomeButtonOnSuccessPaymentResultPage() { }
+
+    @And("User tap Back to Home Button on Failed Payment Result Page")
+    public void userTapBackToHomeButtonOnFailedPaymentResultPage() { }
+}
--- src/test/java/project/stepsdefinition/TopUpStepDefinition.java
+++ src/test/java/project/stepsdefinition/TopUpStepDefinition.java
@@ -2,30 +2,26 @@ package project.stepsdefinition;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import project.pages.TopUpPage;
 
 public class TopUpStepDefinition {
-
+    TopUpPage topUpPage = new TopUpPage();
 
     @When("User input nominal {string} on top up nominal text field on Top Up Page")
-    public void userInputNominalOnTopUpNominalTextFieldOnTopUpPage(String arg0) {
-    }
+    public void userInputNominalOnTopUpNominalTextFieldOnTopUpPage(String nominal) { topUpPage.inputNominal(nominal); }
 
     @When("User tap Pay button on Top Up Page")
-    public void userTapPayButtonOnTopUpPage() { }
+    public void userTapPayButtonOnTopUpPage() { topUpPage.tapPayButton(); }
 
     @Then("User is on Success Payment Result Page")
-    public void userIsOnSuccessPaymentResultPage() {
-    }
+    public void userIsOnSuccessPaymentResultPage() { }
 
     @Then("User is on Failed Payment Result Page")
-    public void userIsOnFailedPaymentResultPage() {
-    }
+    public void userIsOnFailedPaymentResultPage() { }
 
     @Then("User see error message {string} on Top Up Page")
-    public void userSeeErrorMessageOnTopUpPage(String arg0) {
-    }
+    public void userSeeErrorMessageOnTopUpPage(String erMessage) { topUpPage.errorMessage(erMessage); }
 
     @When("User tap Back Button on Top Up Page")
-    public void userTapBackButtonOnTopUpPage() {
-    }
+    public void userTapBackButtonOnTopUpPage() { topUpPage.tapBackButton(); }
 }
